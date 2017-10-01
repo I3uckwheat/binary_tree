a1b347e72dad36484ac7b54cabd94c1eb89d7116
Briggs Elsperger <briggs.elsperger@hotmail.com>
2017-09-30 23:21:23 -0500

    Creates proper binary tree

--- tree.rb
+++ tree.rb
@@ -9,22 +9,22 @@ class Node
 end
 
 def build_tree(array)
-  root = Node.new(array.shift)
+  root = Node.new(array[0])
   right = nil
   left = nil
-  array.each do |value|
+  array[1..-1].each do |value|
     parent = root
     placed = false
     new_node = Node.new(value, parent, left, right)
     until placed
-      if new_node.value <= parent.value
+      if new_node.value >= parent.value
         if parent.right.nil?
           parent.right = new_node
           placed = true
         else
           parent = parent.right
         end
-      elsif new_node.value >= parent.value
+      elsif new_node.value <= parent.value
         if parent.left.nil?
           parent.left = new_node
           placed = true
@@ -37,14 +37,17 @@ def build_tree(array)
   root
 end
 
+def place_node(root, )
+
 def breadth_first_search; end
 
 def depth_first_search; end
 
 def dfs_rec; end
 
-#array_of_data = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+# array_of_data = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
-#build_tree(array_of_data)
+# build_tree(array_of_data)
 
+# repl.it https://repl.it/LqAws/2
 test_ary = [5, 7, 3, 2, 6, 4, 16]
